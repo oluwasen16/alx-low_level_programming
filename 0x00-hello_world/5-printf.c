542f4858024d13fa109ab68ee4e8d604fa4d83a5
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-09 09:09:44 -0700

    prinfts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-/* the description */
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * the program print the output
+ * Return: always 0 (Success)
+ */
+
 int main(void)
+
 {	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
