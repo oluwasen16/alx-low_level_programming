0dc72716ead9b3db6642ec80cb0e312ff96b4f02
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-09 08:35:13 -0700

    printFt

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 	
-	printf("with proper grammar, but the outcome is a piece of art,0\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
 }
 
