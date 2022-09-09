56d344b463215cfd2d1d3e4658ce8466c9da17a6
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-09 08:49:52 -0700

    prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - print the string in the printf function
- * Description: using the main function
- * 
- * Return: Always 0 (successful)
- */ 
+/* the description */
 int main(void)
 {	
 printf("with proper grammar, but the outcome is a piece of art,\n");
