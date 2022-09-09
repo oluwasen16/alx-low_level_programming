1f89c05db5b8f4654af1bbc235f5aefa6b3690db
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-09 14:11:54 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,11 @@
 
 /**
  * main - print the string in the put function
- *
  * Description: using the main function
  * the program print the output
  * Return: always 0 (Success)
  */
-
 int main(void)
-
 {	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
