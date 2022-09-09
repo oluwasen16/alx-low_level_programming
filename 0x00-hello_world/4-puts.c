41c6a8ea86df3d26b9f1b1a57e9d246fabbd481f
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-08 21:09:12 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,12 +8,8 @@
  *
  * Return: 0
  **/ 
-
-
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      
+      puts("\"Programming is like building a multilingual puzzle"); 
       return (0);
-
 }
