89b2a1c08400df074f690a3d47bc0429a53569f1
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-10 07:28:31 +0000

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - Print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzz
-* Return: 0
-*/ 
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
