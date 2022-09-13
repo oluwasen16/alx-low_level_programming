0c3b93dd63f53424980cf0a82172572b7fa123cf
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-13 04:24:52 +0000

    bases

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,17 @@
  */
 int main(void)
 {
-	int c;
-
-	for (c = 48; c <= 57; c++)
+int c;
+for (c = 48; c <= 57; c++)
+{
+	putchar(c);
+	if (c != 57)
 	{
-		putchar(c);
-		if (c != 57)
-		{
-			putchar(',');
-			putchar('');
-
-		}
-	}
-	putchar('\n');
-	return (0);
+	putchar(',');
+	}	
+}	  	
+putchar('\n');
+return (0);
 }
+
 
