bf59b384cf9fc6d5b5c44c3e42b54b2e1fd4cb86
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-13 03:46:17 +0000

    base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	char c;
-	for (c = '0'; c <= '9'; c++)
-	{ 
+char c;
+for (c = '0'; c <= '9'; c++)
+{
 	putchar(c);
-	}
-	for (c = 'a'; c <= 'f'; c++)
-	{
+}
+for (c = 'a'; c <= 'f'; c++)
+{
 	putchar(c);
-	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
