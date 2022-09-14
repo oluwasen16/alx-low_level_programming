1e4957a1515bf1f6466c76bcc4341a7ae19a9fe4
Olawusi Oluwaseun <oluwaseunjoshua29@gmail.com>
2022-09-14 18:12:52 +0000

    lower_c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islower - function to check for small letter character
+ * islower - function to check for small letter character
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
